b08fa73a97cbcb945b86548f24dc035b435804ec
leena988 <151773866+leena988@users.noreply.github.com>
2024-07-08 09:34:16 +0300

    edit register screen

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -2,51 +2,88 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 
-class  registerscreen extends StatelessWidget{
+class  registerscreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('تطبيق التبرع بالدم'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/login');
-              },
-              child: Text('تسجيل الدخول'),
+      body: Stack(
+          children: [
+            Image.asset('assets/Images/intro.png',
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/signup');
-              },
-              child: Text('إنشاء حساب'),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(height: 100,),
+                Text('تبرع', textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'HSI',
+                        fontSize: 60,
+                        color: Color(0xFFAE0E03)
+                    )),
+                Text('بالدم', textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: 'HSI',
+                      fontSize: 90,
+                      color: Color(0xFFAE0E03)
+                  ),
+                ), Text('و أنقذ حياة', textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: 'HSI',
+                      fontSize: 60,
+                      color: Color(0xFFAE0E03)
+                  ),
+                ),
+                SizedBox(height: 30),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    child: Text('تسجيل الدخول', style: TextStyle(
+                        fontFamily: 'HSI',
+                        fontSize: 25,
+                        color: Colors.white
+                    ),),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFAE0E03),
+                        padding: EdgeInsets.only(
+                            right: 25.0, left: 25.0, top: 5.0, bottom: 1.0),
+                        alignment: Alignment.center
+                    )
+                ),
+
+                SizedBox(height: 20),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/signup');
+                    },
+                    child: Text('إنشاء حساب', style: TextStyle(
+                        fontFamily: 'HSI',
+                        fontSize: 25,
+                        color: Colors.white
+                    ),),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFAE0E03),
+                        padding: EdgeInsets.only(
+                            right: 25.0, left: 25.0, top: 5.0, bottom: 1.0),
+                        fixedSize: Size(170, 25)
+                    )
+                ),
+                SizedBox(height: 20),
+                FirebaseAuth.instance.currentUser != null
+                    ? ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/userdata');
+                  },
+                  child: Text('عرض بياناتي'),
+                )
+                    : Container(), // Display button only if user is logged in
+
+              ],
             ),
-            SizedBox(height: 20),
-            FirebaseAuth.instance.currentUser != null
-                ? ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/userdata');
-              },
-              child: Text('عرض بياناتي'),
-            )
-                : Container(), // Display button only if user is logged in
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/map');
-              },
-              child: Text('خريطة البنوك الدموية'),
-            ),
-          ],
-        ),
-      ),
+          ]),
     );
   }
-
-
 }
