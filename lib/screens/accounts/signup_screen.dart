2aad67475752dff87102b9351d2e8e6548e1a37f
lubnasy <lubnasuliman2@gmail.com>
2024-07-25 23:41:02 +0300

    واجهة إنشاء الحساب

--- lib/screens/accounts/signup_screen.dart
+++ lib/screens/accounts/signup_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -26,14 +25,7 @@ class _SignupScreenState extends State<SignupScreen> {
 
   // Ensure default values exist in the dropdown lists
   final List<String> _bloodTypes = [
-    'A+',
-    'A-',
-    'B+',
-    'B-',
-    'AB+',
-    'AB-',
-    'O+',
-    'O-'
+    'A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'
   ];
   final List<String> _genderTypes = ['ذكر', 'انثى'];
 
@@ -66,6 +58,62 @@ class _SignupScreenState extends State<SignupScreen> {
     super.dispose();
   }
 
+  void _showGenderDialog() async {
+    final selectedGender = await showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('اختر الجنس'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: _genderTypes.map((gender) {
+              return ListTile(
+                title: Text(gender),
+                onTap: () {
+                  Navigator.of(context).pop(gender);
+                },
+              );
+            }).toList(),
+          ),
+        );
+      },
+    );
+
+    if (selectedGender != null) {
+      setState(() {
+        _selectedGenderType = selectedGender;
+      });
+    }
+  }
+
+  void _showBloodTypeDialog() async {
+    final selectedBloodType = await showDialog<String>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('اختر فصيلة الدم'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: _bloodTypes.map((bloodType) {
+              return ListTile(
+                title: Text(bloodType),
+                onTap: () {
+                  Navigator.of(context).pop(bloodType);
+                },
+              );
+            }).toList(),
+          ),
+        );
+      },
+    );
+
+    if (selectedBloodType != null) {
+      setState(() {
+        _selectedBloodType = selectedBloodType;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,8 +125,7 @@ class _SignupScreenState extends State<SignupScreen> {
           child: Text(
             'إنشاء حساب ',
             textAlign: TextAlign.right,
-            style:
-                TextStyle(fontFamily: 'HSI', fontSize: 40, color: Colors.black),
+            style: TextStyle(fontFamily: 'HSI', fontSize: 40, color: Colors.black),
           ),
         ),
       ),
@@ -95,111 +142,90 @@ class _SignupScreenState extends State<SignupScreen> {
                   Row(
                     children: [
                       Expanded(
-                        child: _buildTextField(_lastNameController,
-                            'اسم العائلة', 'يرجى إدخال اسم عائلتك'),
+                        child: _buildTextField(_lastNameController, 'اسم العائلة', 'يرجى إدخال اسم عائلتك'),
                       ),
                       const SizedBox(width: 10),
                       Expanded(
-                        child: _buildTextField(_firstNameController,
-                            'الاسم الأول', 'يرجى إدخال اسمك الأول'),
+                        child: _buildTextField(_firstNameController, 'الاسم الأول', 'يرجى إدخال اسمك الأول'),
                       ),
                     ],
                   ),
                   const SizedBox(height: 10),
-                  _buildTextField(
-                      _ssidController, 'الرقم الوطني', 'يرجى إدخال رقمك القومي',
-                      isNumber: true),
+                  _buildTextField(_ssidController, 'الرقم الوطني', 'يرجى إدخال رقمك القومي', isNumber: true),
                   const SizedBox(height: 10),
-                  _buildTextField(_phoneNumberController, 'رقم الهاتف',
-                      'يرجى إدخال رقم هاتفك',
-                      isPhone: true),
+                  _buildTextField(_phoneNumberController, 'رقم الهاتف', 'يرجى إدخال رقم هاتفك', isPhone: true),
                   const SizedBox(height: 10),
-                  _buildDateField(_birthDateController, 'تاريخ الميلاد',
-                      'يرجى إدخال تاريخ ميلادك'),
+                  _buildTextField(_birthDateController, 'تاريخ الميلاد', 'يرجى إدخال تاريخ ميلادك', isDate: true),
                   const SizedBox(height: 10),
                   Row(
                     children: [
                       Expanded(
-                        child: _buildDialogField('فصيلة الدم', _selectedBloodType,
-                            _showBloodTypeDialog),
+                        child: _buildDialogField('فصيلة الدم', _selectedBloodType, _showBloodTypeDialog),
                       ),
                       const SizedBox(width: 10),
                       Expanded(
-                        child: _buildDialogField('الجنس', _selectedGenderType,
-                            _showGenderDialog),
+                        child: _buildDialogField('الجنس', _selectedGenderType, _showGenderDialog),
                       ),
                     ],
                   ),
                   const SizedBox(height: 10),
-                  _buildTextField(_emailController, 'البريد الإلكتروني',
-                      'يرجى إدخال بريدك الإلكتروني',
-                      isEmail: true),
+                  _buildTextField(_emailController, 'البريد الإلكتروني', 'يرجى إدخال بريدك الإلكتروني', isEmail: true),
                   const SizedBox(height: 10),
-                  _buildTextField(_passwordController, 'كلمة المرور',
-                      'يرجى إدخال كلمة المرور',
-                      isPassword: true),
+                  _buildTextField(_passwordController, 'كلمة المرور', 'يرجى إدخال كلمة المرور', isPassword: true),
                   const SizedBox(height: 10),
                 ]),
                 const SizedBox(height: 30),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(175, 45),
-                      backgroundColor: const Color(0xFFAE0E03),
-                      padding: const EdgeInsets.only(
-                          right: 25.0, left: 25.0, top: 5.0, bottom: 1.0),
-                      alignment: Alignment.center),
-                  onPressed: _isButtonDisabled
-                      ? null
-                      : () async {
-                          if (_formKey.currentState!.validate()) {
-                            // Create a new account
-                            setState(() {
-                              _isButtonDisabled = true;
-                            });
-                            try {
-                              UserCredential userCredential =
-                                  await _auth.createUserWithEmailAndPassword(
-                                email: _emailController.text,
-                                password: _passwordController.text,
-                              );
+                    fixedSize: const Size(175, 45),
+                    backgroundColor: const Color(0xFFAE0E03),
+                    padding: const EdgeInsets.only(right: 25.0, left: 25.0, top: 5.0, bottom: 1.0),
+                    alignment: Alignment.center,
+                  ),
+                  onPressed: _isButtonDisabled ? null : () async {
+                    if (_formKey.currentState!.validate()) {
+                      // Create a new account
+                      setState(() {
+                        _isButtonDisabled = true;
+                      });
+                      try {
+                        UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                        );
 
-                              // Save user data in Firestore
-                              await FirebaseFirestore.instance
-                                  .collection('users')
-                                  .doc(userCredential.user!.uid)
-                                  .set({
-                                'firstName': _firstNameController.text,
-                                'lastName': _lastNameController.text,
-                                'email': _emailController.text,
-                                'phoneNumber': _phoneNumberController.text,
-                                'ssid': _ssidController.text,
-                                'birthDate': _birthDateController.text,
-                                'bloodType': _selectedBloodType,
-                                'genderType': _selectedGenderType,
-                              });
+                        // Save user data in Firestore
+                        await FirebaseFirestore.instance.collection('users').doc(userCredential.user!.uid).set({
+                          'firstName': _firstNameController.text,
+                          'lastName': _lastNameController.text,
+                          'email': _emailController.text,
+                          'phoneNumber': _phoneNumberController.text,
+                          'ssid': _ssidController.text,
+                          'birthDate': _birthDateController.text,
+                          'bloodType': _selectedBloodType,
+                          'genderType': _selectedGenderType,
+                        });
 
-                              // Navigate to the donation form screen
-                              Navigator.pushReplacementNamed(context, '/form');
-                            } catch (e) {
-                              if (kDebugMode) {
-                                print('فشل إنشاء الحساب: ${e.toString()}');
-                              }
-                              // Show error message to the user
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                    content: Text('فشل إنشاء الحساب')),
-                              );
-                              setState(() {
-                                _isButtonDisabled = false;
-                              });
-                            }
-                          }
-                        },
+                        // Navigate to the donation form screen
+                        Navigator.pushReplacementNamed(context, '/form');
+                      } catch (e) {
+                        if (kDebugMode) {
+                          print('فشل إنشاء الحساب: ${e.toString()}');
+                        }
+                        // Show error message to the user
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('فشل إنشاء الحساب')),
+                        );
+                        setState(() {
+                          _isButtonDisabled = false;
+                        });
+                      }
+                    }
+                  },
                   child: const Text(
                     'إنشاء حساب',
                     textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontFamily: 'HSI', fontSize: 25, color: Colors.white),
+                    style: TextStyle(fontFamily: 'HSI', fontSize: 25, color: Colors.white),
                   ),
                 ),
               ],
@@ -210,13 +236,8 @@ class _SignupScreenState extends State<SignupScreen> {
     );
   }
 
-  Widget _buildTextField(
-      TextEditingController controller, String labelText, String errorText,
-      {bool isEmail = false,
-      bool isPassword = false,
-      bool isPhone = false,
-      bool isNumber = false,
-      bool isDate = false}) {
+  Widget _buildTextField(TextEditingController controller, String labelText, String errorText,
+      {bool isEmail = false, bool isPassword = false, bool isPhone = false, bool isNumber = false, bool isDate = false}) {
     return Directionality(
         textDirection: TextDirection.rtl,
         child: TextFormField(
@@ -256,47 +277,22 @@ class _SignupScreenState extends State<SignupScreen> {
         ));
   }
 
-  Widget _buildDateField(
-      TextEditingController controller, String labelText, String errorText) {
-    return Directionality(
-      textDirection: TextDirection.rtl,
-      child: InkWell(
-        onTap: () {
-          DatePicker.showDatePicker(context,
-              showTitleActions: true,
-              minTime: DateTime(1900, 1, 1),
-              maxTime: DateTime(2100, 12, 31),
-              onChanged: (date) {
-            controller.text = '${date.year}-${date.month}-${date.day}';
-          }, onConfirm: (date) {
-            controller.text = '${date.year}-${date.month}-${date.day}';
-          }, currentTime: DateTime.now(), locale: LocaleType.ar);
-        },
-        child: IgnorePointer(
-          child: TextFormField(
-            textAlign: TextAlign.right,
-            controller: controller,
-            style: const TextStyle(
-              fontFamily: 'HSI',
-              fontSize: 15,
-              color: Colors.black,
-            ),
-            decoration: InputDecoration(
-              labelStyle: const TextStyle(
-                fontFamily: 'HSI',
-                fontSize: 15,
-                color: Colors.black,
-              ),
-              labelText: labelText,
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              alignLabelWithHint: true,
-            ),
-            validator: (values) {
-              if (values == null || values.isEmpty) {
-                return errorText;
-              }
-              return null;
-            },
+  Widget _buildDialogField(String labelText, String value, Function onTap) {
+    return InkWell(
+      onTap: () => onTap(),
+      child: InputDecorator(
+        decoration: InputDecoration(
+          labelText: labelText,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          alignLabelWithHint: true,
+        ),
+        child: Text(
+          value,
+          textAlign: TextAlign.right,
+          style: const TextStyle(
+            fontFamily: 'HSI',
+            fontSize: 15,
+            color: Colors.black,
           ),
         ),
       ),
@@ -304,110 +300,38 @@ class _SignupScreenState extends State<SignupScreen> {
   }
 
   Widget _buildInfoSection(String title, List<Widget> children) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Align(
-          alignment: Alignment.centerRight,
-          child: Text(
-            title,
-            style: const TextStyle(
-                fontFamily: 'HSI', fontSize: 20, color: Colors.black),
-          ),
-        ),
-        const SizedBox(height: 10),
-        ...children,
-      ],
-    );
-  }
-
-  Widget _buildDialogField(String title, String currentValue, Function onTap) {
     return Directionality(
-      textDirection: TextDirection.rtl,
-      child: InkWell(
-        onTap: () => onTap(),
-        child: InputDecorator(
-          decoration: InputDecoration(
-            labelText: title,
-            labelStyle: const TextStyle(
-              fontFamily: 'HSI',
-              fontSize: 15,
-              color: Colors.black,
-            ),
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+        textDirection: TextDirection.rtl,
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: [
+              const BoxShadow(
+                color: Color.fromRGBO(112, 112, 112, 100),
+                blurRadius: 5,
+              ),
+            ],
           ),
-          child: Text(
-            currentValue,
-            style: const TextStyle(
-              fontFamily: 'HSI',
-              fontSize: 15,
-              color: Colors.black,
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontFamily: 'HSI',
+                  fontSize: 25,
+                ),
+              ),
+              const Divider(
+                height: 22,
+                thickness: 1.5,
+              ),
+              ...children,
+            ],
           ),
-        ),
-      ),
-    );
-  }
-
-  void _showBloodTypeDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('اختر فصيلة الدم', textAlign: TextAlign.right),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: _bloodTypes
-                  .map(
-                    (bloodType) => GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _selectedBloodType = bloodType;
-                        });
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(
-                        bloodType,
-                        textAlign: TextAlign.right,
-                      ),
-                    ),
-                  )
-                  .toList(),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  void _showGenderDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('اختر الجنس', textAlign: TextAlign.right),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: _genderTypes
-                  .map(
-                    (genderType) => GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _selectedGenderType = genderType;
-                        });
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(
-                        genderType,
-                        textAlign: TextAlign.right,
-                      ),
-                    ),
-                  )
-                  .toList(),
-            ),
-          ),
-        );
-      },
-    );
+        ));
   }
 }
