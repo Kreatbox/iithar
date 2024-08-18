0d3ef183431717ff913a06103a3db33d6935f396
leena988 <151773866+leena988@users.noreply.github.com>
2024-08-18 12:21:05 +0300

    final splash screen

--- lib/screens/first_run/splash_screen.dart
+++ lib/screens/first_run/splash_screen.dart
@@ -1,26 +1,39 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-
-import 'intro1_screen.dart';
 import 'onboarding_screen.dart';
 
-class SplashScreen extends StatelessWidget{
+class SplashScreen extends StatefulWidget{
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return AnimatedSplashScreen(
-      splash: Column(
-        children: [
-          Center(
-            child: Lottie.asset('assets/Animation/icon99.json'),
-          )
-        ],
-      ) ,nextScreen: Onboarding(),
-      duration: 3500,
-      backgroundColor: Colors.white,
-    );
-  }
+  State<SplashScreen> createState() => _SplashScreenState();
+
 }
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 8), () {
+      Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => Onboarding()));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Lottie.asset('assets/Animation/icon99.json',),
+          SizedBox(height: 20,),
+        ],
+
+      ),);
+  }
+
+}
+
